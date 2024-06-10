8d2ab0a18aba84749bf2239b07206bc0c19be682
DanielRtF11 <daniel.rt.f11@gmail.com>
2024-06-10 23:37:05 +0200

    casi todo acabado

--- src/componentes/reusables/SelectMultimedia.jsx
+++ src/componentes/reusables/SelectMultimedia.jsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import { FaVideo } from "react-icons/fa";
 import { IoMdImages } from "react-icons/io";
 import { IoMusicalNotesSharp } from "react-icons/io5";
+import '../styles.css';  // Asegúrate de que este archivo contenga las clases CSS necesarias
 
 const SelectMultimedia = ({ setActiveSection }) => {
+    const [selectedIcon, setSelectedIcon] = useState('');  // Estado para rastrear el icono seleccionado
+
+    const handleIconClick = (section) => {
+        setActiveSection(section);
+        setSelectedIcon(section);  // Actualiza el icono seleccionado
+    };
+
     return (
         <div className="iconosContenedor">
-            <IoMdImages className="icono" onClick={() => setActiveSection('images')} />
-            <FaVideo className="icono" onClick={() => setActiveSection('videos')} />
-            <IoMusicalNotesSharp className='icono' onClick={() => setActiveSection('audio')} />
+            <IoMdImages
+                className={`icono ${selectedIcon === 'images' ? 'iconoSelect' : ''}`}
+                onClick={() => handleIconClick('images')}
+            />
+            <FaVideo
+                className={`icono ${selectedIcon === 'videos' ? 'iconoSelect' : ''}`}
+                onClick={() => handleIconClick('videos')}
+            />
+            <IoMusicalNotesSharp
+                className={`icono ${selectedIcon === 'audio' ? 'iconoSelect' : ''}`}
+                onClick={() => handleIconClick('audio')}
+            />
         </div>
     );
-}
+};
 
 export default SelectMultimedia;
